1390e8e091047fcc4d30b2b8615a54087ca00693
Arto Kalishian <r2@Artos-MacBook-Air.local>
2026-01-28 22:40:49 +0200

    chore: ignore and remove build artifacts from _dev-system/analyzer/target

--- _dev-system/analyzer/target/debug/deps/zmij-0d9dd192e68c2b5d.d
+++
@@ -1,8 +0,0 @@
-/Users/r2/Desktop/robust-virtual-tour-builder/_dev-system/analyzer/target/debug/deps/zmij-0d9dd192e68c2b5d.d: /Users/r2/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zmij-1.0.17/src/lib.rs /Users/r2/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zmij-1.0.17/src/traits.rs
-
-/Users/r2/Desktop/robust-virtual-tour-builder/_dev-system/analyzer/target/debug/deps/libzmij-0d9dd192e68c2b5d.rlib: /Users/r2/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zmij-1.0.17/src/lib.rs /Users/r2/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zmij-1.0.17/src/traits.rs
-
-/Users/r2/Desktop/robust-virtual-tour-builder/_dev-system/analyzer/target/debug/deps/libzmij-0d9dd192e68c2b5d.rmeta: /Users/r2/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zmij-1.0.17/src/lib.rs /Users/r2/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zmij-1.0.17/src/traits.rs
-
-/Users/r2/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zmij-1.0.17/src/lib.rs:
-/Users/r2/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zmij-1.0.17/src/traits.rs:
--- _dev-system/analyzer/target/release/deps/zmij-9fa59b967fb79640.d
+++
@@ -1,8 +0,0 @@
-/Users/r2/Desktop/robust-virtual-tour-builder/_dev-system/analyzer/target/release/deps/zmij-9fa59b967fb79640.d: /Users/r2/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zmij-1.0.17/src/lib.rs /Users/r2/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zmij-1.0.17/src/traits.rs
-
-/Users/r2/Desktop/robust-virtual-tour-builder/_dev-system/analyzer/target/release/deps/libzmij-9fa59b967fb79640.rlib: /Users/r2/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zmij-1.0.17/src/lib.rs /Users/r2/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zmij-1.0.17/src/traits.rs
-
-/Users/r2/Desktop/robust-virtual-tour-builder/_dev-system/analyzer/target/release/deps/libzmij-9fa59b967fb79640.rmeta: /Users/r2/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zmij-1.0.17/src/lib.rs /Users/r2/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zmij-1.0.17/src/traits.rs
-
-/Users/r2/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zmij-1.0.17/src/lib.rs:
-/Users/r2/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zmij-1.0.17/src/traits.rs:
